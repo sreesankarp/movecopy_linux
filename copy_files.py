3f00f945fa1a7139bbef0e3b2a9a4eadcdc2dab1
Sree Sankar P <37147746+sreesankarp@users.noreply.github.com>
2022-08-03 13:44:28 +0530

    Update copy_files.py

--- copy_files.py
+++ copy_files.py
@@ -8,9 +8,9 @@ if _source_path[-1]!="/":
     _source_path+="/"
 _mode = (input("Enter 'c' for copy 'm' to move : ")).lower()
 
-_total_files = os.listdir(_destination_path)
+_total_files = os.listdir(_source_path)
 _total_len = int(len(_total_files))
-_confirm_prompt = (input(f"Do you want to continue to {'COPY' if _mode=='c' else 'MOVE'} - {_total_len} files from {_destination_path} to {_source_path} (y/n) : ")).lower()
+_confirm_prompt = (input(f"Do you want to continue to {'COPY' if _mode=='c' else 'MOVE'} - {_total_len} files from {_source_path} to {_destination_path} (y/n) : ")).lower()
 
 if _confirm_prompt=='y':
     _checkpoint = int(input(f"Enter checkpoint range to display progress for {_total_len} files : "))
